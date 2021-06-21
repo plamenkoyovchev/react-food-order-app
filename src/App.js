18c1f0d48666b09e9d90456c8ed68b76567f2e3d
plamenkoyovchev <plamenkooo@gmail.com>
2021-06-21 22:43:11 +0300

    Add initial modal visibity handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+
+  const [showCart, setShowCart] = useState(false);
+
+  const showCartHandler = () => setShowCart(true);
+
+  const hideCartHandler = () => setShowCart(false);
+
   return (
     <Fragment>
-      <Header />
+      <Header onShowCart={showCartHandler} />
       <main>
+        {showCart && <Cart onClose={hideCartHandler} />}
         <Meals />
       </main>
     </Fragment>
