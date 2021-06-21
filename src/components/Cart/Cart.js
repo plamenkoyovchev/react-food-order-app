02f80c6dd1e348bec4b5fcc92c08e2a6377c1365
plamenkoyovchev <plamenkooo@gmail.com>
2021-06-21 22:42:21 +0300

    Add missing key

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,27 +1,30 @@
 import React from "react";
+import Modal from "../UI/Modal/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
 	const cartItems = (
 		<ul className={classes["cart-items"]}>
 			{[{ id: "c1", name: "Sushi", amount: 2, price: 19.99 }].map((item) => (
-				<li>{item.name}</li>
+				<li key={item.id}>{item.name}</li>
 			))}
 		</ul>
 	);
 
 	return (
-		<div>
+		<Modal>
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
 				<span>12.99</span>
 			</div>
 			<div className={classes.actions}>
-				<button className={classes["button--alt"]}>Close</button>
+				<button className={classes["button--alt"]} onClick={props.onClose}>
+					Close
+				</button>
 				<button className={classes.button}>Order</button>
 			</div>
-		</div>
+		</Modal>
 	);
 };
 
