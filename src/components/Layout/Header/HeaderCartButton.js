00ae502c9a2199e26a9be83991bbacf4ad174654
plamenkoyovchev <plamenkooo@gmail.com>
2021-06-22 09:37:56 +0300

    Get number of cart items

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 import CartIcon from "../../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = ({ onClick }) => {
+    const cartContext = useContext(CartContext);
+    const numberOfCartItems = cartContext.items
+        .reduce((currentSum, item) => currentSum + item.amount, 0);
+
     return (
         <button className={classes.button} onClick={onClick}>
             <span className={classes.icon}>
@@ -12,7 +17,7 @@ const HeaderCartButton = ({ onClick }) => {
                 Your cart
             </span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     );
