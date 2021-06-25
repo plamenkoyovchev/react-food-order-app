d9f2a52c8219de8af3b24a4f5ef02ebdb5ffde79
plamenkoyovchev <plamenkooo@gmail.com>
2021-06-25 17:43:41 +0300

    Add bump effect to HeaderCartButton component

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,26 +1,46 @@
-import React, { useContext } from 'react';
-import CartContext from '../../../store/cart-context';
+import React, { useContext, useEffect, useState } from "react";
+import CartContext from "../../../store/cart-context";
 import CartIcon from "../../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = ({ onClick }) => {
-    const cartContext = useContext(CartContext);
-    const numberOfCartItems = cartContext.items
-        .reduce((currentSum, item) => currentSum + item.amount, 0);
+	const [itemHighlighted, setItemHighlighted] = useState(false);
 
-    return (
-        <button className={classes.button} onClick={onClick}>
-            <span className={classes.icon}>
-                <CartIcon />
-            </span>
-            <span>
-                Your cart
-            </span>
-            <span className={classes.badge}>
-                {numberOfCartItems}
-            </span>
-        </button>
-    );
+	const cartContext = useContext(CartContext);
+	const { items } = cartContext;
+
+	const numberOfCartItems = items.reduce(
+		(currentSum, item) => currentSum + item.amount,
+		0
+	);
+
+	let btnClasses = `${classes.button} ${itemHighlighted ? classes.bump : ""}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+
+		setItemHighlighted(true);
+
+		const timer = setTimeout(() => {
+			setItemHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
+	return (
+		<button className={btnClasses} onClick={onClick}>
+			<span className={classes.icon}>
+				<CartIcon />
+			</span>
+			<span>Your cart</span>
+			<span className={classes.badge}>{numberOfCartItems}</span>
+		</button>
+	);
 };
 
 export default HeaderCartButton;
