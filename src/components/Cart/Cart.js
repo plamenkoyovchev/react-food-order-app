1eea5fa8d49233d1d481bdae81538eae89c9bcb9
plamenkoyovchev <plamenkooo@gmail.com>
2021-06-25 17:20:30 +0300

    Implement add and remove cart item feature

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,36 @@
 import React, { useContext } from "react";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 import Modal from "../UI/Modal/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
 	const cartContext = useContext(CartContext);
-	const { items, totalAmount } = cartContext;
+	const { items, totalAmount, addItem, removeItem } = cartContext;
 
 	const hasItems = items.length > 0;
 
+	const onAddHandler = (item) =>
+		addItem({
+			...item,
+			amount: 1,
+		});
+
+	const onRemoveHandler = (id) => removeItem(id);
+
 	const cartItems = (
 		<ul className={classes["cart-items"]}>
-			{items && items.map((item) => <li key={item.id}>{item.name}</li>)}
+			{items &&
+				items.map((item) => (
+					<CartItem
+						key={item.id}
+						name={item.name}
+						amount={item.amount}
+						price={item.price}
+						onAdd={() => onAddHandler(item)}
+						onRemove={() => onRemoveHandler(item.id)}
+					/>
+				))}
 		</ul>
 	);
 
