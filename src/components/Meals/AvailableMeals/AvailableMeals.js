eaa4479d2c87364af217ee3a8c15ba6fa839c520
plamenkoyovchev <plamenkooo@gmail.com>
2021-07-05 09:47:39 +0300

    Add loading and error state

--- src/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -5,24 +5,54 @@ import MealItem from "../MealItem/MealItem";
 
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+
 	useEffect(() => {
 		const fetchMeals = async () => {
-			const response = await fetch('https://foodapp-a0f2e-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+			const response = await fetch(
+				"https://foodapp-a0f2e-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+			);
+
+			if (!response.ok) {
+				throw new Error("Couldn't fetch data...");
+			}
+
 			const data = await response.json();
 			const mealItems = [];
 			for (const key in data) {
 				const meal = data[key];
 				mealItems.push({
 					id: key,
-					...meal
+					...meal,
 				});
 			}
 
 			setMeals(mealItems);
+			setLoading(false);
 		};
 
-		fetchMeals();
+		fetchMeals().catch((error) => {
+			setLoading(false);
+			setError(error);
+		});
 	}, []);
+
+	if (loading) {
+		return (
+			<section className={classes["available-meals-loading"]}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+	if (error) {
+		return (
+			<section className={classes["available-meals-error"]}>
+				<p>{error}</p>
+			</section>
+		);
+	}
 
 	const mealItems = meals.map((meal) => (
 		<MealItem
