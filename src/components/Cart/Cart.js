00cee9845f86674e60ba9f77a68b27a3ec0f1b2a
plamenkoyovchev <plamenkooo@gmail.com>
2021-07-10 22:51:06 +0300

    Clear cart items on successful order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,9 +7,11 @@ import Checkout from "../Checkout/Checkout";
 
 const Cart = ({ onClose }) => {
 	const cartContext = useContext(CartContext);
-	const { items, totalAmount, addItem, removeItem } = cartContext;
+	const { items, totalAmount, addItem, removeItem, clearCart } = cartContext;
 
 	const [isCheckout, setIsCheckout] = useState(false);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [didSubmit, setDidSubmit] = useState(false);
 
 	const hasItems = items.length > 0;
 
@@ -24,18 +26,24 @@ const Cart = ({ onClose }) => {
 	const orderHandler = () => setIsCheckout(true);
 
 	const submitOrderHandler = async (userData) => {
+		setIsSubmitting(true);
 		try {
-			await fetch('https://foodapp-a0f2e-default-rtdb.europe-west1.firebasedatabase.app/orders.json', {
-				method: 'POST',
-				body: JSON.stringify({
-					user: userData,
-					orderedItems: items
-				})
-			});
-		} catch (error) {
+			await fetch(
+				"https://foodapp-a0f2e-default-rtdb.europe-west1.firebasedatabase.app/orders.json",
+				{
+					method: "POST",
+					body: JSON.stringify({
+						user: userData,
+						orderedItems: items,
+					}),
+				}
+			);
 
+			clearCart();
+		} catch (error) {
 		} finally {
-			onClose();
+			setIsSubmitting(false);
+			setDidSubmit(true);
 		}
 	};
 
@@ -55,8 +63,8 @@ const Cart = ({ onClose }) => {
 		</ul>
 	);
 
-	return (
-		<Modal>
+	const cartModalContent = (
+		<>
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
@@ -66,9 +74,44 @@ const Cart = ({ onClose }) => {
 				<button className={classes["button--alt"]} onClick={onClose}>
 					Close
 				</button>
-				{hasItems && <button onClick={orderHandler} className={classes.button}>Order</button>}
+				{hasItems && (
+					<button onClick={orderHandler} className={classes.button}>
+						Order
+					</button>
+				)}
 			</div>
-			{isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={onClose} />}
+			{isCheckout && (
+				<Checkout onConfirm={submitOrderHandler} onCancel={onClose} />
+			)}
+		</>
+	);
+
+	const isSubmittingModalContent = (
+		<>
+			<p>Sending order data...</p>
+			<div className={classes.actions}>
+				<button className={classes.button} onClick={onClose}>
+					Close
+				</button>
+			</div>
+		</>
+	);
+	const isSubmittedModalContent = (
+		<>
+			<p>Order submitted successfully.</p>
+			<div className={classes.actions}>
+				<button className={classes["button--alt"]} onClick={onClose}>
+					Close
+				</button>
+			</div>
+		</>
+	);
+
+	return (
+		<Modal>
+			{!isSubmitting && !didSubmit && cartModalContent}
+			{isSubmitting && isSubmittingModalContent}
+			{!isSubmitting && didSubmit && isSubmittedModalContent}
 		</Modal>
 	);
 };
