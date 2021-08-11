47f51cbadeaf1119344dbcdfb4e80fda31466792
plamenkoyovchev <plamenkooo@gmail.com>
2021-08-11 21:39:57 +0300

    Add reducers in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./slices/cartSlice";
+import mealReducer from "./slices/mealsSlice";
+import uiReducer from "./slices/uiSlice";
 
 export const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        meals: mealReducer,
+        ui: uiReducer
     }
 });
